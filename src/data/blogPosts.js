aa2e30d84e456a8de21017ac9fbfa64b50f9d3c8
Adam Schwartz <adam@saahomes.com>
2026-08-02 13:12:44 +0000

    Add YouTube video to blog post: closing-day-colorado-guide

--- src/data/blogPosts.js
+++ src/data/blogPosts.js
@@ -4378,6 +4378,7 @@ export const blogPosts = [
     category: 'Buyer Tips',
     image: '/images/blog-default-black.jpg',
     readTime: '6 min read',
+    youtubeId: '2PAv4hZ5pbQ',
     relatedLinks: [
       { title: 'Colorado Home Buyers Guide', href: '/for-buyers/', description: 'Expert buyer representation & resources' },
       { title: 'CHFA Down Payment Assistance Guide', href: '/chfa-down-payment-assistance/', description: 'Grants up to $25,000 for Colorado buyers' },
